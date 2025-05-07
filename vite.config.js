170173842900d59d90331880fcc9ad163417d941
Serhii Nikolin <serhii.nikolin@headworks.com.ua>
2025-05-07 18:04:57 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue__list-of-posts/',
   plugins: [
     vue(),
     vueDevTools(),
